994a1efe29eb40a650c4722550fc4e0ab1b2b98a
wandl <hnxxyhcwdl1003@163.com>
2021-03-06 23:48:36 +0800

    xx

--- src/main/java/com/github/dozermapper/extra/converters/number/BigDecimalStringConverter.java
+++ src/main/java/com/github/dozermapper/extra/converters/number/BigDecimalStringConverter.java
@@ -4,33 +4,24 @@ import java.math.BigDecimal;
 
 import org.apache.commons.beanutils.converters.BigDecimalConverter;
 
-import com.github.dozermapper.core.CustomConverter;
-import com.github.dozermapper.core.MappingException;
+import com.github.dozermapper.core.DozerConverter;
 
-public final class BigDecimalStringConverter implements CustomConverter {
+public final class BigDecimalStringConverter extends DozerConverter<BigDecimal, String> {
 	
 	private final BigDecimalConverter converter = new BigDecimalConverter();
 	
-	/**
-	 * 转换接口实现 
-	 * @param destinationFieldValue：目标字段值
-	 * @param sourceFieldValue：源字段值
-	 * @param destinationClass:目标字段类型
-	 * @param sourceClass：源字段类型
-	 * @return 转换后的结果
-	 */
-	public Object convert(Object destinationFieldValue, Object sourceFieldValue, Class<?> destinationClass, Class<?> sourceClass) {
-		if (sourceFieldValue == null) {
-			return null;
-		}
-		//String to BigDecimal
-		if (sourceClass.equals(String.class)&&sourceFieldValue instanceof String) {
-			return converter.convert(String.class, sourceFieldValue);
-		}
-		//BigDecimal to String
-		if (sourceClass.equals(BigDecimal.class)&&sourceFieldValue instanceof BigDecimal ) {
-			return ((BigDecimal) sourceFieldValue).toPlainString();
-		}
-		throw new MappingException( "Converter DateStringConverter used incorrectly. Arguments passed in were:" + destinationFieldValue + " and " + sourceFieldValue);
+	public BigDecimalStringConverter() {
+		super(BigDecimal.class, String.class);
 	}
+	
+	@Override
+	public BigDecimal convertFrom(String source, BigDecimal destination) {
+		return converter.convert(BigDecimal.class, source);
+	}
+
+	@Override
+	public String convertTo(BigDecimal source, String destination) {
+		return source.toPlainString();
+	}
+	
 }
--- src/main/java/com/github/dozermapper/extra/converters/number/BigIntegerStringConverter.java
+++ src/main/java/com/github/dozermapper/extra/converters/number/BigIntegerStringConverter.java
@@ -4,33 +4,24 @@ import java.math.BigInteger;
 
 import org.apache.commons.beanutils.converters.BigIntegerConverter;
 
-import com.github.dozermapper.core.CustomConverter;
-import com.github.dozermapper.core.MappingException;
+import com.github.dozermapper.core.DozerConverter;
 
-public final class BigIntegerStringConverter implements CustomConverter {
+public final class BigIntegerStringConverter extends DozerConverter<BigInteger, String> {
 	
 	private final BigIntegerConverter converter = new BigIntegerConverter();
      
-	/**
-	 * 转换接口实现 
-	 * @param destinationFieldValue：目标字段值
-	 * @param sourceFieldValue：源字段值
-	 * @param destinationClass:目标字段类型
-	 * @param sourceClass：源字段类型
-	 * @return 转换后的结果
-	 */
-	public Object convert(Object destinationFieldValue, Object sourceFieldValue, Class<?> destinationClass, Class<?> sourceClass) {
-		if (sourceFieldValue == null) {
-			return null;
-		}
-		//String to BigInteger
-		if (sourceClass.equals(String.class)&&sourceFieldValue instanceof String) {
-			return converter.convert(String.class, sourceFieldValue);
-		}
-		//BigInteger to String
-		if (sourceClass.equals(BigInteger.class)&&sourceFieldValue instanceof BigInteger) {
-			return ((BigInteger) sourceFieldValue).toString();
-		}
-		throw new MappingException( "Converter BigIntegerStringConverter used incorrectly. Arguments passed in were:" + destinationFieldValue + " and " + sourceFieldValue);
+	public BigIntegerStringConverter() {
+		super(BigInteger.class, String.class);
 	}
+	
+	@Override
+	public BigInteger convertFrom(String source, BigInteger destination) {
+		return converter.convert(BigInteger.class, source);
+	}
+
+	@Override
+	public String convertTo(BigInteger source, String destination) {
+		return source.toString();
+	}
+	
 }
